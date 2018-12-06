6fc0e8dee12929be66ba21f6e4f522b0cef356e2
mockmango7 <mockmango7@gmail.com>
2018-12-06 22:27:03 +0100

    Some changes

--- GarantieMonitor/server.js
+++ GarantieMonitor/server.js
@@ -9,6 +9,16 @@ http.createServer(function (req, res) {
     res.end('Hello World\n');
 
     var q = url.parse(req.url, true);
-    console.log(q.pathname);
+    var filename = "./HTML" + q.pathname;
+    console.log(filename);
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
     
 }).listen(port);
