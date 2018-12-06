0ae641603879ab02219a5137588b4c82bc6ac450
lenardjee <lenardvdmaas@hotmail.nl>
2018-12-06 17:12:18 +0100

    new files

--- GarantieMonitor/server.js
+++ GarantieMonitor/server.js
@@ -1,8 +1,14 @@
 'use strict';
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+
+    var q = url.parse(req.url, true);
+    console.log(q.pathname);
+    
 }).listen(port);
