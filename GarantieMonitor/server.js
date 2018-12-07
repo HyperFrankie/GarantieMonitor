56a11dfd9e18a0816e6d29134cc46c1a1dcbd7ef
lenardjee <lenardvdmaas@hotmail.nl>
2018-12-07 17:06:34 +0100

    Changed structure and added 404 page

--- GarantieMonitor/server.js
+++ GarantieMonitor/server.js
@@ -5,20 +5,39 @@ var fs = require('fs');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-
     var q = url.parse(req.url, true);
-    var filename = "./HTML" + q.pathname;
-    console.log(filename);
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found");
+    if (q.pathname === "/" || q.pathname === "" || q.pathname === "/favicon.ico") {
+        console.log("q is not correct")
+    } else {
+        var path = "./HTML" + q.pathname;
+        console.log(path);
+
+        var fileContents = readFile(path);
+        if (fileContents == undefined) {
+            console.log("file contents of " + path + "undefined");
+            res.write(readFile("./HTML/PageNotFound404.html").toString);
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            console.log(fileContents);
+            res.write(fileContents);
+            res.end();
         }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-    
+
+        function readFile(path) {
+            console.log("reading file " + path);
+            fs.readFile(path, 'utf8', function (err, fileContents) {
+                if (err) {
+                    console.log("An error occured while loading \'" + path + "\': ");
+                    throw err;
+                } else if (fileContents) {
+                    console.log(fileContents);
+                    return fileContents.toString();
+                }
+                console.log("finished trying to load HTML file from " + path);
+                return undefined;
+            });
+        }
+
+    }
 }).listen(port);
